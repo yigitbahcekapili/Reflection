10cb05326e61dd47ebd49af7a2d45edfa60da905
Yiğit <yigit_fcb@hotmail.com>
2018-10-28 03:14:38 +0300

    Initial commit

---
+++ LolHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    /// <summary>
+    /// LolHero classı
+    /// </summary>
+    public class LolHero
+    {
+        public List<int> MyList { get; set; }
+        public int[] MyNumbers { get; set; }
+        public string[] MyCharacters { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double NumberPi { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        // Goto : GetCollectValue Fonksiyonu recursive yazılacak
+        static void Main(string[] args)
+        {
+            LolHero Vayne = new LolHero();
+            #region Property
+            Vayne.MyList = new List<int> { 1, 2, 3 };
+            Vayne.MyNumbers = new int[] { 1, 2, 3 };
+            Vayne.MyCharacters = new string[] { "a", "b", "c" };
+            Vayne.Id = 26;
+            Vayne.Name = "Best Adc";
+            Vayne.NumberPi = 3.14;
+            #endregion
+
+            var prop = Vayne.GetType().GetProperties();
+            bool[] IsCollection = IsICollectionArray(prop);
+            int i = 0;
+            foreach (var item in prop)
+            {
+                Console.Write(item.Name + " : ");
+                if (IsCollection[i] == true)
+                {
+                    GetCollectionValue((ICollection)item.GetValue(Vayne));
+                }
+                else
+                {
+                    Console.Write(Vayne.GetType().GetProperty(item.Name).GetValue(Vayne, null));
+                    Console.WriteLine("\n");
+                }
+
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Collection Tipindeki nesnelerin içindeki değerleri yaz
+        /// </summary>
+        /// <param name="a"></param>
+        public static void GetCollectionValue(ICollection a)
+        {
+            foreach (var item in a)
+            {
+                Console.Write(item + " , ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        /// <summary>
+        /// Class'ın propertylerinin ICollection sınıfının alt sınıfı olup olmadığını kontrol eder
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static bool[] IsICollectionArray(PropertyInfo[] prop)
+        {
+            Type ICollectionType = typeof(ICollection);
+            bool[] boolList = new bool[prop.Length];
+            int i = 0;
+            foreach (var item in prop)
+            {
+                boolList[i] = (ICollectionType.IsAssignableFrom(item.PropertyType));
+                i++;
+            }
+            return boolList;
+        }
+    }
+}
